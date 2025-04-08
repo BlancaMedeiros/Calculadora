68f2a69ba547d8d0ec6027ff8e10db441f8f57cd
BlancaMedeiros <biancamedeiroslabiapari@gmail.com>
2025-04-08 16:24:05 -0300

    Definição de parâmetros para função "calcular".

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
-def calcular():
+def calcular(n1,operacao,n2):
     if operacao == '+':
         resultado = n1 + n2
     elif operacao == '-':
@@ -7,14 +7,15 @@ def calcular():
         resultado = n1 * n2
     elif operacao == '/':
         if n2 == 0:
-            print("Erro: não é possível dividir por zero!")
-            return
+            return "Erro! não é possível dividir por zero."
         resultado = n1 / n2
     else:
         print("Opção inválida")
     return resultado
 
-n1 = float(input("Digite um valor: "))
+numero_01 = float(input("Digite um valor: "))
 operacao = input("diga qual operação: ")
-n2 = float(input("digite mais um valor: "))
-print('o resultado da operação é: ', calcular())
+numero_02 = float(input("digite mais um valor: "))
+
+
+print('o resultado da operação é: ', calcular(numero_01,operacao,numero_02))
